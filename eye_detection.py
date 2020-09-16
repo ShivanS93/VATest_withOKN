3d33b2452f133c8fc11b8e111b947cf71cdb5be4
Shivan <shivan@shivansivakumaran.com>
2020-09-16 20:59:26 +1200

    created eye tracking

--- eye_detection.py
+++ eye_detection.py
@@ -3,9 +3,43 @@
 # tutorial: https://www.roytuts.com/real-time-eye-detection-in-webcam-using-python-3/
 
 import cv2
+import math
+import numpy as np
 
-faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
-eyeCascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+def main():
 
-# grab the reference to the webcam
-vs = cv2.VideoCapture(0)
+    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+    eyeCascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+    # grab the reference to the webcam
+    vs = cv2.VideoCapture(0)
+
+        while True:
+            ret, frame = vs.read()
+
+            if frame is None:
+                break
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gray = cv2.equalizeHist(gray)
+
+            faces = faceCascade.detectMultiScale(frame)
+
+            for (x, y, w, h) in faces:
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = frame[y:y+h, x:x+w]
+                eyes = eyeCascade.detectMultiScale(roi_gray)
+                for (ex,ey,ew,eh) in eyes:
+                    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
+
+                cv2.imshow("Video", frame)
+                key = cv2.waitKey(1) & 0xFF
+
+                if key == ord('q') or key == 27:
+                    break
+
+
+        cv2.destroyWindows()
+
+
+if __name__ == '__main__': main()
